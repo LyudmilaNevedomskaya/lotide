3050cbaba8f5a22d15914d4ce43632ca2c079641
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-07-21 22:26:05 +0000

    Counting letters in a sentence and creating new object

---
+++ countLetters.js
@@ -0,0 +1,17 @@
+const countLetters = function(sentence) {
+  let newSent = sentence.split(' ').join('');
+
+  let result = {};
+
+  for (let letter of newSent) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  } console.log(result);
+  
+
+};
+
+countLetters('lighthouse in the house');
