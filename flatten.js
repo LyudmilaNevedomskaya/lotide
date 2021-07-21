8b59cdb382907da28c40b81c1d219f17a7ed5bd7
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-07-21 06:05:44 +0000

    looping nested arrays

---
+++ flatten.js
@@ -0,0 +1,18 @@
+const flatten = function(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "object") {
+      for (let j = 0; j < arr[i].length; j++) {
+        newArr.push(arr[i][j]);
+        
+      }
+    } else {
+      newArr.push(arr[i]);
+    }
+    
+  }
+  console.log(newArr);
+  
+};
+
+flatten([1, 2, [3, 4], 5, [6]]);
