2b1f0ec92e1a93f613d51909899537a3df73be92
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-07-21 01:22:27 +0000

    Assert Arrays Equality

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function(arr1, arr2) {
+  let bool = true;
+  let count = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      count++;
+    }
+  }
+  if (count !== arr1.length) {
+    bool = false;
+  }
+  if (bool) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
+  } else {
+    console.log(`⛔⛔⛔ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+eqArrays(['1', '2', '4'], ['1', '2', '45']);
