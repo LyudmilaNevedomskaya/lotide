fe87b0d35d9e2abdfc9f5b344534e84a3f0eac1e
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-07-21 21:53:40 +0000

    Objects countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,47 @@
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const result = {};
+
+  for (let name of allItems) {
+    if (itemsToCount[name]) {
+      if (result[name]) {
+        result[name] += 1;
+      } else {
+        result[name] = 1;
+      }
+    }
+  } return result;
+
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+console.log(result1);
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍 👍 👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
