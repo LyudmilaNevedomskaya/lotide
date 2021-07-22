241402641cbed708fd36709e99abebb2a4eee0e5
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-07-22 04:35:18 +0000

    Compare objects

--- eqObjects.js
+++ eqObjects.js
@@ -19,8 +19,8 @@ const eqObjects = function(object1, object2) {
     return false;
   }
 };
-const ab = { c: "1", b: "2" };
-const ba = { b: "2", a: "1" };
+const ab = { a: ["1"], b: "2" };
+const ba = { b: "2", a: ["1"] };
 console.log(eqObjects(ab, ba)); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
