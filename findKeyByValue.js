8a886a266a9b547c921d2109e29cdb86d4a06f91
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-07-22 03:34:40 +0000

    Find Key by Value

---
+++ findKeyByValue.js
@@ -0,0 +1,20 @@
+const findKeyByValue = function(show, name) {
+
+  for (let key in show) {
+    if (show[key] === name) {
+      return key;
+    }
+
+  } return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+
+
+console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"));
