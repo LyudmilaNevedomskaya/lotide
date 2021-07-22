f29d02343c98b8681c7047e35e0e1f5cb2644dd7
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-07-22 04:59:02 +0000

    Middle array

---
+++ middle.js
@@ -0,0 +1,19 @@
+const middle = function(array) {
+  let result = [];
+  let middleArray;
+  if (array.length <= 2) {
+    return result;
+  } else {
+    if (array.length % 2 === 0) {
+      middleArray = array.length / 2;
+      result.push(array[middleArray - 1], array[middleArray]);
+    } else {
+      middleArray = Math.floor(array.length / 2);
+      result.push(array[middleArray]);
+    }
+  } return result;
+};
+
+console.log(middle([1, 3]));
+console.log(middle([2]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
