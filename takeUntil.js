11f00958cd83238137ce429842ddaf960632dead
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-07-23 00:44:47 +0000

    Implement takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -1,9 +1,22 @@
 const takeUntil = function(array, callback) {
-  let result = array.filter(function(item) {
-    return item.callback(x)
+  let newArr = [];
 
-  })
-}
+  for (let item of array) {
+    if (callback(item)) {
+      break;
+    } else {
+      newArr.push(item);
+    }
+    
+  }
+    
+  return newArr;
+};
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
+
 console.log(results1);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
