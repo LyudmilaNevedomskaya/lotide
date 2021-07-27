74c02d17f750e5799904fac51978ead1a45954e7
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-07-27 00:40:52 +0000

    Added assertEqual with mocha

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,25 @@
+const assert = require('chai').assert;
+const assertEqual = require('../assertEqual')
 
-const assertEqual = require('../assertEqual');
+describe ('assertion equal or not', () => {
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+  it ('return false for "Lighthouse Labs" and "Bootcamp"', () => {
+    assert.isFalse(assertEqual("Lighthouse Labs", "Bootcamp"), true)
+  });
+
+  it ('return true for 1 and 1', () => {
+    assert.isTrue(assertEqual(1, 1), true)
+  });
+
+  it ('return true for "Mila" and "Mila"', () => {
+    assert.isTrue(assertEqual('Mila', 'Mila'), true)
+  });
+
+});
+
+
+// const assertEqual = require('../assertEqual');
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
 
